09d4a8d347f218de614c570089bd59be9420ece5
Jose Gonzalez Riquelme <jozho.g@gmail.com>
2020-05-21 19:06:40 -0400

    Muestra todo desde el archivo terminado

--- ReservaSalaCine/Form3.Designer.cs
+++ ReservaSalaCine/Form3.Designer.cs
@@ -33,13 +33,21 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btn_muestratodo = new System.Windows.Forms.Button();
+            this.btn_busca = new System.Windows.Forms.Button();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(210, 89);
+            this.label1.Location = new System.Drawing.Point(115, 89);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(117, 13);
             this.label1.TabIndex = 0;
@@ -49,7 +57,7 @@
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(182, -7);
+            this.pictureBox1.Location = new System.Drawing.Point(99, -7);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(170, 93);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -73,12 +81,75 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "Ingrese Rut";
             // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 197);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(436, 430);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Rut";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Inicio Sesion";
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Fin Sesion";
+            this.columnHeader3.Width = 100;
+            // 
+            // btn_muestratodo
+            // 
+            this.btn_muestratodo.Location = new System.Drawing.Point(28, 157);
+            this.btn_muestratodo.Name = "btn_muestratodo";
+            this.btn_muestratodo.Size = new System.Drawing.Size(122, 23);
+            this.btn_muestratodo.TabIndex = 5;
+            this.btn_muestratodo.Text = "MOSTRAR TODO";
+            this.btn_muestratodo.UseVisualStyleBackColor = true;
+            this.btn_muestratodo.Click += new System.EventHandler(this.btn_muestratodo_Click);
+            // 
+            // btn_busca
+            // 
+            this.btn_busca.Location = new System.Drawing.Point(179, 157);
+            this.btn_busca.Name = "btn_busca";
+            this.btn_busca.Size = new System.Drawing.Size(75, 23);
+            this.btn_busca.TabIndex = 6;
+            this.btn_busca.Text = "BUSCAR";
+            this.btn_busca.UseVisualStyleBackColor = true;
+            this.btn_busca.Click += new System.EventHandler(this.btn_busca_Click);
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Accion";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "AccionF";
+            this.columnHeader5.Width = 100;
+            // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(524, 346);
+            this.ClientSize = new System.Drawing.Size(461, 639);
+            this.Controls.Add(this.btn_busca);
+            this.Controls.Add(this.btn_muestratodo);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.pictureBox1);
@@ -98,5 +169,13 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button btn_muestratodo;
+        private System.Windows.Forms.Button btn_busca;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
     }
 }
--- ReservaSalaCine/Form3.cs
+++ ReservaSalaCine/Form3.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,53 @@ namespace ReservaSalaCine
 {
     public partial class Form3 : Form
     {
+        CLASEEVALUA2josegonzalez usuario = new CLASEEVALUA2josegonzalez();
+        private string archivoruta = @"C:\TXTS\VIGIAJOSEGONZALEZ.txt";
+        List<CLASEEVALUA2josegonzalez> muestra = new List<CLASEEVALUA2josegonzalez>();
+
         public Form3()
         {
             InitializeComponent();
+        }
+
+        private void btn_muestratodo_Click(object sender, EventArgs e)
+        {
+            StreamReader leer = new StreamReader(archivoruta);
+            string linea;
+            linea = leer.ReadLine();
+            while (linea != null)
+            {
+                string[] fecha;
+                string[] datos = linea.Split(',');
+                for (int i = 0; i < datos.Length; i += 5)
+                {
+                    fecha = datos[i + 4].Split(';');
+                    CLASEEVALUA2josegonzalez usuario = new CLASEEVALUA2josegonzalez(datos[i], Convert.ToDateTime(datos[i + 1]),
+                        Convert.ToDateTime(datos[i + 2]), datos[i+3], Convert.ToDateTime(fecha[0]));
+                    muestra.Add(usuario);
+                }
+                linea = leer.ReadLine();
+            }
+            leer.Close();
+            mostrar(muestra);
+        }
+
+        private void btn_busca_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void mostrar(List<CLASEEVALUA2josegonzalez> lista)
+        {
+            foreach (CLASEEVALUA2josegonzalez dato in lista)
+            {
+                ListViewItem al = new ListViewItem(dato.Rut);
+                al.SubItems.Add(dato.InicioSesion.ToString());
+                al.SubItems.Add(dato.finSesion.ToString());
+                al.SubItems.Add(dato.Accion);
+                al.SubItems.Add(dato.AccionF.ToString());
+                listView1.Items.Add(al);
+            }
         }
     }
 }
